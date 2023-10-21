720a8e2132abf89fc927ad2218d0d598cfd44818
Aiden Gimpel <aiden.gimpel@ontariotechu.net>
2023-10-20 21:07:57 -0400

    Updated version

---
+++ GD2023/Assets/Scripts/Enemies/Enemy_Base.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Base : MonoBehaviour
+{
+    [SerializeField] int _tier;
+    [SerializeField] int _maxHealth;
+    [SerializeField] int _moveSpeed;
+    [SerializeField] int _atkDamage;
+
+
+}
