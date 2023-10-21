9eb91b77268b8a5ebab358162e3e967ffe0011ab
Tyler Wong <tywong8@gmail.com>
2023-10-21 13:22:16 -0400

    button and blood paint transition

---
+++ GD2023/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach(Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Play Menu Music
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+
+    }
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Stop();
+    }
+
+    public void ChangeMusicVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+}
